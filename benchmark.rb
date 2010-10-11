b92bc145fa3f04dfe83021d5f61a9a0ba8c72be5
Adam Meehan <adam.meehan@gmail.com>
2010-10-12 10:00:22 +1100

    cleanup benchmarks

--- benchmark.rb
+++ benchmark.rb
@@ -1,75 +1,72 @@
 $:.unshift(File.expand_path('lib'))
 
 require 'benchmark'
-require 'date'
 require 'parsedate'
-require 'rubygems'
-
-require 'timeliness'
+require 'lib/timeliness'
 
 n = 10000
 Benchmark.bm do |x|
-  x.report('timeliness - datetime') { 
+  x.report('timeliness - datetime') {
     n.times do
       Timeliness::Parser.parse("2000-01-04 12:12:12", :datetime)
-    end 
+    end
   }
 
-  x.report('timeliness - date') { 
+  x.report('timeliness - date') {
     n.times do
       Timeliness::Parser.parse("2000-01-04", :date)
-    end 
+    end
   }
-  
-  x.report('timeliness - date as datetime') { 
+
+  x.report('timeliness - date as datetime') {
     n.times do
       Timeliness::Parser.parse("2000-01-04", :datetime)
-    end 
+    end
   }
 
-  x.report('timeliness - time') { 
+  x.report('timeliness - time') {
     n.times do
       Timeliness::Parser.parse("12:01:02", :time)
-    end 
+    end
   }
-  
-  x.report('timeliness - invalid format datetime') { 
+
+  x.report('timeliness - invalid format datetime') {
     n.times do
       Timeliness::Parser.parse("20xx-01-04 12:12:12", :datetime)
-    end 
+    end
   }
 
-  x.report('timeliness - invalid format date') { 
+  x.report('timeliness - invalid format date') {
     n.times do
       Timeliness::Parser.parse("20xx-01-04", :date)
-    end 
+    end
   }
 
-  x.report('timeliness - invalid format time') { 
+  x.report('timeliness - invalid format time') {
     n.times do
       Timeliness::Parser.parse("12:xx:02", :time)
-    end 
+    end
   }
 
-  x.report('timeliness - invalid value datetime') { 
+  x.report('timeliness - invalid value datetime') {
     n.times do
       Timeliness::Parser.parse("2000-01-32 12:12:12", :datetime)
-    end 
+    end
   }
 
-  x.report('timeliness - invalid value date') { 
+  x.report('timeliness - invalid value date') {
     n.times do
       Timeliness::Parser.parse("2000-01-32", :date)
-    end 
+    end
   }
 
   x.report('timeliness - invalid value time') {
     n.times do
       Timeliness::Parser.parse("12:61:02", :time)
-    end 
+    end
   }
 
-  x.report('Rails fast date/time') { 
+  x.report('Rails fast date/time') {
     n.times do
       "2000-01-04 12:12:12" =~ /\A(\d{4})-(\d{2})-(\d{2}) (\d{2})[\. :](\d{2})([\. :](\d{2}))?\Z/
       arr = [$1, $2, $3, $3, $5, $6].map {|i| i.to_i }
@@ -78,41 +75,41 @@ Benchmark.bm do |x|
     end
   }
 
-  x.report('Date._parse - valid') { 
+  x.report('Date._parse - valid') {
     n.times do
       Date._parse("2000-01-04 12:12:12")
-    end 
+    end
   }
 
-  x.report('Date._parse - invalid ') { 
+  x.report('Date._parse - invalid ') {
     n.times do
       Date._parse("2000-01-32 12:12:12")
-    end 
+    end
   }
-  
-  x.report('parsedate - valid') { 
+
+  x.report('parsedate - valid') {
     n.times do
       arr = ParseDate.parsedate("2000-01-04 12:12:12")
       Date.new(*arr[0..2])
       Time.mktime(*arr)
-    end 
+    end
   }
 
-  x.report('parsedate - invalid ') { 
+  x.report('parsedate - invalid ') {
     n.times do
       arr = ParseDate.parsedate("2000-00-04 12:12:12")
-    end 
-  }
-  
-  x.report('strptime - valid') { 
-    n.times do
-      DateTime.strptime("2000-01-04 12:12:12", '%Y-%m-%d %H:%M:%s')
-    end 
+    end
   }
 
-  x.report('strptime - invalid') { 
+  x.report('strptime - valid') {
+    n.times do
+      DateTime.strptime("2000-01-04 12:12:12", '%Y-%m-%d %H:%M:%s')
+    end
+  }
+
+  x.report('strptime - invalid') {
     n.times do
       DateTime.strptime("2000-00-04 12:12:12", '%Y-%m-%d %H:%M:%s') rescue nil
-    end 
+    end
   }
 end
