12fef7c10329e7412b166577ac95f7f69f926efe
Adam Meehan <adam.meehan@gmail.com>
2010-10-11 19:32:46 +1100

    tidying

--- lib/timeliness/format_set.rb
+++ lib/timeliness/format_set.rb
@@ -4,58 +4,60 @@ module Timeliness
 
     attr_reader :formats, :regexp
 
-    def self.compile(formats)
-      set = new(formats)
-      set.compile!
-      set
-    end
+    class << self
+      def compile(formats)
+        set = new(formats)
+        set.compile!
+        set
+      end
 
-    def self.compile_format(string_format)
-      format = string_format.dup
-      format.gsub!(/([\.\\])/, '\\\\\1') # escapes dots and backslashes
-      found_tokens, token_order, value_token_count = [], [], 0
+      def compile_format(string_format)
+        format = string_format.dup
+        format.gsub!(/([\.\\])/, '\\\\\1') # escapes dots and backslashes
+        found_tokens, token_order, value_token_count = [], [], 0
 
-      # Substitute tokens with numbered placeholder
-      Parser.sorted_token_keys.each do |token|
-        regexp_str, arg_key = *Parser.format_tokens[token]
-        if format.gsub!(/#{token}/, "%<#{found_tokens.size}>")
-          if arg_key
-            regexp_str = "(#{regexp_str})"
-            value_token_count += 1
+        # Substitute tokens with numbered placeholder
+        Parser.sorted_token_keys.each do |token|
+          regexp_str, arg_key = *Parser.format_tokens[token]
+          if format.gsub!(/#{token}/, "%<#{found_tokens.size}>")
+            if arg_key
+              regexp_str = "(#{regexp_str})"
+              value_token_count += 1
+            end
+            found_tokens << [regexp_str, arg_key]
           end
-          found_tokens << [regexp_str, arg_key]
         end
+
+        # Replace placeholders with token regexps
+        format.scan(/%<(\d)>/).each {|token_index|
+          token_index = token_index.first
+          regexp_str, arg_key = found_tokens[token_index.to_i]
+          format.gsub!("%<#{token_index}>", regexp_str)
+          token_order << arg_key
+        }
+
+        define_format_method(string_format, token_order.compact)
+        return format, value_token_count
+      rescue
+        raise "The following format regular expression failed to compile: #{format}\n from format #{string_format}."
       end
 
-      # Replace placeholders with token regexps
-      format.scan(/%<(\d)>/).each {|token_index|
-        token_index = token_index.first
-        regexp_str, arg_key = found_tokens[token_index.to_i]
-        format.gsub!("%<#{token_index}>", regexp_str)
-        token_order << arg_key
-      }
-
-      define_format_method(token_order.compact, string_format)
-      return format, value_token_count
-    rescue
-      raise "The following format regular expression failed to compile: #{format}\n from format #{string_format}."
-    end
-
-    # Compiles a format method which maps the regexp capture groups to method
-    # arguments based on order captured. A time array is built using the argument
-    # values placed in the position defined by the component.
-    #
-    def self.define_format_method(components, name)
-      values = [nil] * 7
-      components.each do |component|
-        position, code = *Parser.format_components[component]
-        values[position] = code || "#{component}.to_i" if position
-      end
-      class_eval <<-DEF
-        define_method(:"format_#{name}") do |#{components.join(',')}|
-          [#{values.map {|i| i || 'nil' }.join(',')}]
+      # Compiles a format method which maps the regexp capture groups to method
+      # arguments based on order captured. A time array is built using the argument
+      # values placed in the position defined by the component.
+      #
+      def define_format_method(name, components)
+        values = [nil] * 7
+        components.each do |component|
+          position, code = *Parser.format_components[component]
+          values[position] = code || "#{component}.to_i" if position
         end
-      DEF
+        class_eval <<-DEF
+          define_method(:"format_#{name}") do |#{components.join(',')}|
+            [#{values.map {|i| i || 'nil' }.join(',')}]
+          end
+        DEF
+      end
     end
 
     def initialize(formats)
