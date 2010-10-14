7a964c428ad13208df2f71bac9560553ad22bccf
Adam Meehan <adam.meehan@gmail.com>
2010-10-14 14:51:01 +1100

    expose _parse method on Timeliness

--- lib/timeliness.rb
+++ lib/timeliness.rb
@@ -10,7 +10,7 @@ require 'timeliness/version'
 module Timeliness
   class << self
     extend Forwardable
-    def_delegators Parser, :parse
+    def_delegators Parser, :parse, :_parse
     def_delegators Formats, :add_formats, :remove_formats, :use_us_formats, :use_euro_formats
     attr_accessor :default_timezone, :date_for_time_type, :ambiguous_year_threshold
   end
