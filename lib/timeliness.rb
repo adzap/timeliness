fe36d2ac7dfa62a4a2a75f0dbb3e0dfabb25a250
Carlos Figueiredo <carlos.figueiredo87@gmail.com>
2019-04-25 17:38:27 -0400

    Add delegator for Timeliness::Definition.add_format_tokens

--- lib/timeliness.rb
+++ lib/timeliness.rb
@@ -13,14 +13,14 @@ module Timeliness
   class << self
     extend Forwardable
     def_delegators Parser, :parse, :_parse
-    def_delegators Definitions, :add_formats, :remove_formats, :use_us_formats, :use_euro_formats
+    def_delegators Definitions, :add_formats, :remove_formats, :use_us_formats, :use_euro_formats, :add_format_tokens
     attr_accessor :default_timezone, :date_for_time_type, :ambiguous_year_threshold
   end
 
   # Default timezone. Options:
   #   - :local (default)
   #   - :utc
-  #   
+  #
   #   If ActiveSupport loaded, also
   #   - :current
   #   - 'Zone name'
